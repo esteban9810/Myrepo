47b69e1803b765e6171fcc3b38717113c0e572d7
esteban9810 <esteban_9810@outlook.es>
2019-03-15 12:02:07 -0500

    creacion scripts Php

---
+++ scripts_ejemplos/script1.php
@@ -0,0 +1,14 @@
+<center>Programaciòn Avanzada</center>
+<?php
+  $nombre= "Peter";
+  $apellido= "Watson";
+  $edad= 30;
+  $salario= 800;
+
+  echo "<center>Bienvenidos<center>";
+  echo "Nombre:" .$nombre ."<br>";
+  echo "Apellido:" .$apellido ."<br>";
+  echo "Edad:" .$edad ."<br>";
+  echo "Salario:" .$salario ."<br>";
+?>
+<center>Fin del Programa</center>
---
+++ scripts_ejemplos/script2.php
@@ -0,0 +1,13 @@
+
+
+<?php
+  $num1= 10;
+  $num2= 5;
+
+  echo "<center>";
+  echo "La suma es:" .($num1 + $num2) ."<br>";
+  echo "La resta es:" .($num1 - $num2) ."<br>";
+  echo "La multiplicacion es:" .$num1 * $num2 ."<br>";
+  echo "La divison es:" .$num1 / $num2 ."<br>";
+  echo "</center>";
+?>
